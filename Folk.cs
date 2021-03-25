bd32fb6740ea0a0b3d8742c90fc91f43fe3a516c
Kristian <kristian.risvaer@gmail.com>
2021-03-25 15:14:19 +0100

    Initial commit

---
+++ Folk.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Parametere
+{
+    public class Folk
+    {
+        public List<string> MyBitches;
+
+
+        public Folk(params string[] mybiches)
+        {
+            MyBitches = new List<string>(mybiches);
+        }
+
+        // Metode som tar imot en parameter typ string og ser om den finnes i listen MyBitches og skriver ut hallo til den bitchen
+
+        public string FindBitch(string bitch)
+        {
+            foreach (var t in MyBitches)
+            {
+                if (t == bitch)
+                {
+                    return $"Du er min bitch {t}";
+                }
+            }
+            return $"Fant ingen bitch med navn {bitch}. Vil du bli min bitch?";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+namespace Parametere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SiHallo("Henrik");
+            SiHallo("Erik");
+            SiHallo("Flemming");
+            SiHallo("Tobias");
+            SiHallo("Sindre");
+            SiHallo("Stian");
+            Console.WriteLine();
+
+            HundreX(1);
+            HundreX(69);
+            HundreX(420);
+            HundreX(1337);
+            Console.WriteLine();
+
+            // Uten params nøkkelordet ville det sett sånn ut:
+            // var listeMedBitches = new Folk(new string[] {"Henrik", "Flemming", "Stian", "Erik", "Sindre", "Tobias"})
+
+            var listeMedBitches = new Folk("Henrik", "Flemming", "Stian", "Erik", "Sindre", "Tobias", "Sigve");
+
+            var bitch = listeMedBitches.FindBitch("Kjell");
+            Console.WriteLine(bitch);
+
+            var bitch2 = listeMedBitches.FindBitch("Stian");
+            Console.WriteLine(bitch2);
+        }
+            // Metode som tar imot en parameter typ string og skriver dette i consollen
+        static void SiHallo(string navn)
+        {
+            Console.WriteLine($"Hallo {navn} :D");
+        }
+
+        // Metode som tar imot en parameter typ int og skriver ut tallet ganger 100
+
+        static void HundreX(int tall)
+        {
+            Console.WriteLine(tall * 100);
+        }
+    }
+}
